dd514f47ad2765e02340b0aafe6aee217f64243a
stefan <stefansprenger@gmx.at>
2017-03-30 10:03:10 +0200

    Coin Poof implemented

--- Assets/Udacity/Scripts/Coin.cs
+++ Assets/Udacity/Scripts/Coin.cs
@@ -6,7 +6,14 @@ public class Coin : MonoBehaviour
 {
     //Create a reference to the CoinPoofPrefab
 
+    public GameObject puffObject;
+    public AudioSource puffSound;
+
     public void OnCoinClicked() {
+        Vector3 coinPosition = transform.position;
+        Object.Instantiate(puffObject, coinPosition, Quaternion.Euler(-90, 0, 0));
+        puffSound.Play();
+        Destroy(this.gameObject);
         // Instantiate the CoinPoof Prefab where this coin is located
         // Make sure the poof animates vertically
         // Destroy this coin. Check the Unity documentation on how to use Destroy
